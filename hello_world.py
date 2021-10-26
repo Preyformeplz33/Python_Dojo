91adf64f963246589cc44754582183a309bafa79
Preyformeplz33 <patterson.eric.ep@gmail.com>
2021-10-25 22:25:32 -0500

    first commit

---
+++ hello_world.py
@@ -0,0 +1,23 @@
+# 1. TASK: print "Hello World"
+print( "Hello World" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Noelle"
+print( 'Hello' , name )	# with a comma
+# print( your code here )	# with a +
+print ( 'Hello'+ name )
+# # 3. print "Hello 42!" with the number in a variable
+num = 42
+print('Hello ' + str(num))
+# name = 42
+name = 42
+# print( your code here )	# with a comma
+print ('Hello', name)
+# print( your code here )	# with a +	-- this one should give us an error!
+# print('Hello' + name)
+# # 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+# print( your code here ) # with .format()
+print(" I love to eat {} and {}.".format (fave_food1, fave_food2))
+# print( your code here ) # with an f string
+print(f" I  love to {fave_food1} and {fave_food2} eat.")
