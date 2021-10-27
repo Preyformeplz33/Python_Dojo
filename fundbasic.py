6409d78eb3173d35c2679af5cb1b45be66c144fd
Preyformeplz33 <patterson.eric.ep@gmail.com>
2021-10-27 00:09:14 -0500

    first commit

---
+++ fundbasic.py
@@ -0,0 +1,156 @@
+def number_of_food_groups():
+    return 5
+print(number_of_food_groups())
+# return the number 5 and quit functions
+
+
+#2
+#2
+# def number_of_military_branches():
+#     return 5
+# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+# NameError: name 'number_of_days_in_a_week_silicon_or_triangle_sides' is not defined
+# Why did i get this error?
+
+#3
+# def number_of_books_on_hold():
+#     return 5
+#     return 10
+# print(number_of_books_on_hold())
+# this function returned 5
+
+#4
+# def number_of_fingers():
+#     return 5
+#     print(10)
+# print(number_of_fingers())
+# this function returned 5 is the same as js?
+
+# #5
+# def number_of_great_lakes():
+#     print(5)
+# x = number_of_great_lakes()
+# print(x)
+# will pring the number 5 then print x which is none becase its empty.
+
+#6
+# def add(b,c):
+#     print(b+c)
+# print(add(1,2) + add(2,3))
+# it would add 3 + 4
+# got an unsupported operator error message
+
+#7
+# def concatenate(b,c):
+#     return str(b)+str(c)
+# ="support function python from-rainbow">print(concatenate(2,5))
+#  ="support function python from-rainbow">print(concatenate(2,5))
+#     ^
+
+#8
+# def number_of_oceans_or_fingers_or_continents():
+#     b = 100
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+#         return 10
+#     return 7
+# print(number_of_oceans_or_fingers_or_continents())
+#   File "fundbasic.py", line 56
+#         else:
+#            ^
+# SyntaxError: invalid character in identifier
+
+
+#9
+# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+#     if b<c:
+#         return 7
+#     else:
+#         return 14
+#     return 3
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+#  i got 7 14 and 21
+
+
+#10
+# def addition(b,c):
+#     return b+c
+#     return 10
+# print(addition(3,5))
+
+# I got an 8 , After return function ends huh?
+
+#11
+# b = 500
+# print(b)
+# def foobar():
+#     b ="keyword operator from-rainbow">= 300
+#     print(b)
+# print(b)
+# foobar()
+# print(b)
+# this gave me an error message also why ?
+# File "fundbasic.py", line 94, in <module>
+#     foobar()
+#     File "fundbasic.py", line 91, in foobar
+#     b ="keyword operator from-rainbow">= 300
+# TypeError: '>=' not supported between instances of 'str' and 'int
+
+#12
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# foobar()
+# print(b)
+# line 105 print 500 106 print 500 109 print 300
+#110 wont run because return quits a function
+
+#13
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# b=foobar()
+# print(b)
+# line 124 define foo bar as 500? 
+
+#14
+# def foo():
+#     print(1)
+#     bar()
+#     print(2)
+# def bar():
+#     print(3)
+# foo()
+# python will look for to see if something is defined before printing? or returning?
+
+
+#15
+# def foo():
+#     print(1)
+#     x = bar()
+#     print(x)
+#     return 10
+# def bar():
+#     print(3)
+#     return 5
+# y = foo()
+# print(y)
+# prints 1 x = bar bar is a funtcion that prints 3  next line returns 10 then ends cuz of return?
+
+
+
+
+
+
