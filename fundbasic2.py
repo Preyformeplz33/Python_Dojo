2b7db8bc7340141730736f1b13e29f89af710479
Preyformeplz33 <patterson.eric.ep@gmail.com>
2021-10-27 08:23:36 -0500

    first commit

---
+++ fundbasic2.py
@@ -0,0 +1,42 @@
+# def reduce(num):
+# # output is used when you want to print multiple times
+#     output = []
+#     for m in range(num,-1 ,-1):
+#         output.append(m)
+#     return output
+# print(reduce(5))   
+# append is how you add or create a list using items? [] makes something a list
+
+# def print_and_return(list):
+#     print(list[0])
+#     return list [1]
+# print(print_and_return([3,3]))
+# #only works for 2 numbers or first 2 in a list
+
+# def first_plus_length(list):
+#     return list[0] + len(list)
+# print(first_plus_length([3,3,3,4,4]))
+#print calls the function , even if its under return
+
+# def values_greater_than_second(list):
+#     if len(list) < 2:
+#         return False
+#     output = []
+#     for i in range(0,len(list)):
+#         if list[i] > list [1]:
+#             output.append(list[i])
+#     print(len(output))
+#     return output
+# print(values_greater_than_second([5,2,3,2,1,4]))
+# print(values_greater_than_second([3]))
+
+# def length_and_value(length,value):
+#     output = []
+#     for i in range(0, length):
+#         output.append(value)
+#     return output
+
+# print(length_and_value(3,3))
+# print(length_and_value(3,float('inf')))
+
+# hmm range 0 is where it begins the length is where it ends oh i see the length is the first number , oh okay
