c0b403a9ab531a7ab78e4a1a4a7f275c275529cc
Preyformeplz33 <patterson.eric.ep@gmail.com>
2021-10-27 15:14:32 -0500

    bonus commit

--- bankaccount.py
+++ bankaccount.py
@@ -5,34 +5,33 @@ class User:
     
     def make_withdrawal(self,amount):
         self.balance -= amount
+        return self
 
     def display_user_balance(self):
         print (self.balance)
     def dep(self,amount):
         self.balance += amount
-    def trans(self,amount,user)
+        return self
+    def trans(self,amount,user):
         self.balance += amount 
         user.balance -= amount
         print("transfer complete")
+        user.display_user_balance()
+        self.display_user_balance()
 
 userone =User(500)
-userone.dep(33)
-userone.dep(33)
-userone.dep(33)
+userone.dep(33).dep(33).dep(33)
 userone.make_withdrawal(5)
 userone.display_user_balance()
 
 usertoo=User(333)
+usertoo.dep(3).dep(3)
 usertoo.dep(3)
-usertoo.dep(3)
-usertoo.make_withdrawal(2)
-usertoo.make_withdrawal(2)
+usertoo.make_withdrawal(2).make_withdrawal(2)
 usertoo.display_user_balance()
 
 userthree=User(1000)
 userthree.dep(200)
 userthree.trans(33,userone)
-userthree.make_withdrawal(1)
-userthree.make_withdrawal(1)
-userthree.make_withdrawal(1)
+userthree.make_withdrawal(1).make_withdrawal(1).make_withdrawal(1)
 userthree.display_user_balance()
